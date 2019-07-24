ad37f4b16c330f1371e35ee178d812d031863c96
ChrisTimperley <christimperley@googlemail.com>
2019-07-23 22:44:29 -0400

    added setup.py

--- pyroglyph/__init__.py
+++ pyroglyph/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: utf-8 -*-
 __all__ = ('__version__',)
-__version__ = 0.0.1
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+PACKAGE_NAME = 'pyroglyph'
+VERSION = 0.0.1
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    python_requires='>=3.5',
+    description='A simple library for building real-time text-based UIs',
+    long_description=open('README.rst').read(),
+    author='Chris Timperley',
+    author_email='ctimperley@cmu.edu',
+    url='https://github.com/ChrisTimperley/pyroglyph',
+    license='Apache License 2.0',
+    install_requires=[
+        'typing-extensions>=3.7.2'
+    ],
+    packages=['pyroglyph'],
+    keywords=['cli', 'blessed', 'ui', 'simple'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
