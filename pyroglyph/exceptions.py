7642f4e459ccf558b4a0073dd1e10ae3d0669452
ChrisTimperley <christimperley@googlemail.com>
2019-07-25 15:51:59 -0400

    added new exceptions

--- pyroglyph/exceptions.py
+++ pyroglyph/exceptions.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
-__all__ = ('__version__', 'PyroglyphException')
+__all__ = ('__version__', 'PyroglyphException', 'AlreadyStarted',
+           'AlreadyTerminated')
 
 
 class PyroglyphException(Exception):
     """Base class used by all Pyroglyph exceptions."""
+
+
+class AlreadyStarted(PyroglyphException):
+    """The render loop has already been started."""
+
+
+class AlreadyTerminated(PyroglyphException):
+    """The render loop has already been terminated."""
